948110dcae1571dfc41fc9b164e522e14d0134a8
Alex Coronell <alexcoronell@gmail.com>
2024-09-26 15:46:31 -0500

    Student Entity created

---
+++ src/main/java/com/alexcoronell/school_management/persistence/entity/StudentEntity.java
@@ -0,0 +1,47 @@
+package com.alexcoronell.school_management.persistence.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "student")
+@Getter
+@Setter
+@NoArgsConstructor
+public class StudentEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(name = "first_name", nullable = false, length = 100)
+    private String firstName;
+
+    @Column(name = "middle_name", length = 100)
+    private String middleName;
+
+    @Column(name = "last_name", nullable = false, length = 100)
+    private String lastName;
+
+    @Column(name = "second_last_name", length = 100)
+    private String secondLastName;
+
+    @Column(name = "date_of_birth", nullable = false, columnDefinition = "DATE")
+    private LocalDateTime dateOfBirth;
+
+    @Column(nullable = false)
+    private String gender;
+
+    @Column(columnDefinition = "DATE")
+    private LocalDateTime enrolment;
+
+    @Column(columnDefinition = "SMALLINT")
+    private Boolean active;
+
+    @Column(columnDefinition = "SMALLINT")
+    private Boolean deleted;
+}
